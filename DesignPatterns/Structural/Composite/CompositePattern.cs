4462d08f0f1c3854a721394f7ee9c3d7a05a7c3a
Ankit Bajpai <ravi.mandala.ext@flydubai.com>
2018-12-30 00:41:37 +0530

    Added composite design pattern examples

---
+++ DesignPatterns/Structural/Composite/CompositePattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Structural.Composite.Standard
+{
+    public interface IComponent
+    {
+        void Operation1();
+        void Operation2();
+    }
+
+    public class Leaf : IComponent
+    {
+        public void Operation1()
+        {
+            Console.WriteLine("Operation 1");
+        }
+
+        public void Operation2()
+        {
+            Console.WriteLine("Operation 2");
+        }
+    }
+
+    public class Composite : IComponent
+    {
+        List<IComponent> components = new List<IComponent>();
+
+        public void Operation1()
+        {
+            Console.WriteLine("Operation 1");
+        }
+
+        public void Operation2()
+        {
+            Console.WriteLine("Operation 2");
+        }
+
+        public void Add(IComponent component)
+        {
+            components.Add(component);
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Composite/CompositePatternRealWorldDemo1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Structural.Composite.RealWorld
+{
+    public interface IFileComponent
+    {
+        void Delete();
+        void PrintName();
+    }
+
+    public class LeafFile : IFileComponent
+    {
+        string fileName;
+
+        public LeafFile(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("File Deleted");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("File name: " + this.fileName);
+        }
+    }
+
+    public class Directory : IFileComponent
+    {
+        string directoryName;
+        List<IFileComponent> filesOrFolders = new List<IFileComponent>();
+
+        public Directory(string directoryName)
+        {
+            this.directoryName = directoryName;
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Directory Deleted");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Directory name: " + this.directoryName);
+        }
+
+        public void Add(IFileComponent fileOrFolder)
+        {
+            this.filesOrFolders.Add(filesOrFolder);
+        }
+
+
+    }
+}
